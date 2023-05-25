42e9d83fc18af0e38363fa33a3e51f95821c38e6
yejiabin9 <1147357312@qq.com>
2023-05-25 23:13:08 +0800

    init common

---
+++ config.go
@@ -0,0 +1,22 @@
+package common
+
+import (
+	"github.com/asim/go-micro/plugins/config/source/consul/v3"
+	"github.com/asim/go-micro/v3/config"
+	"strconv"
+)
+
+func GetConsulConfig(host string, port int64, prefix string) (config.Config, error) {
+	consulSource := consul.NewSource(
+		consul.WithAddress(host+":"+strconv.FormatInt(port, 10)),
+		consul.WithPrefix(prefix),
+		consul.StripPrefix(true),
+	)
+	conf, err := config.NewConfig()
+	if err != nil {
+		return conf, err
+	}
+	err = conf.Load(consulSource)
+	return conf, err
+
+}
