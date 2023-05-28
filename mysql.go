cfa1d078b85aa29a785cbd1e167aa94d36dd53c0
yejiabin9 <1147357312@qq.com>
2023-05-28 20:56:37 +0800

    add:add mysql config

---
+++ mysql.go
@@ -0,0 +1,17 @@
+package common
+
+import "github.com/asim/go-micro/v3/config"
+
+type MysqlConfig struct {
+	Host     string `json:"host"`
+	User     string `json:"user"`
+	Pwd      string `json:"pwd"`
+	Database string `json:"database"`
+	Port     string `json:"port"`
+}
+
+func GetMysqlFromConsul(config config.Config, path ...string) *MysqlConfig {
+	mysqlConfig := &MysqlConfig{}
+	config.Get(path...).Scan(mysqlConfig)
+	return mysqlConfig
+}
